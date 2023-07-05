091bc317f236ad7e2d376113ad47ff857c7c9845
Shibli Noman <nomanshiblisunny@gmail.coom>
2023-07-05 11:38:55 +0600

    Sign In Page Design Done

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:eduhome_project/screens/authenticate/sign_in.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignIn(),
+    );
+  }
+}
